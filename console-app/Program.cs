c66e2bdc16bbd297a1daa3dcec03635c4968161e
Caroline Chao <44160165+caracha1@users.noreply.github.com>
2022-11-09 14:51:53 +0100

    Add fake token

--- console-app/Program.cs
+++ console-app/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! happy developer 2");
-
+string token="123456789012345678901234567890";
+Console.WriteLine(token);
